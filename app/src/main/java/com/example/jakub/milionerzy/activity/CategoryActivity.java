d133c9e1d73e4ca513320c1d312d8d389e437bab
Jakub Kuśnierz <jakubkusnierz@gmail.com>
2018-10-31 10:56:29 +0100

    http request to category/get

--- app/src/main/java/com/example/jakub/milionerzy/activity/CategoryActivity.java
+++ app/src/main/java/com/example/jakub/milionerzy/activity/CategoryActivity.java
@@ -1,27 +1,23 @@
 package com.example.jakub.milionerzy.activity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.Spinner;
 
-import com.example.jakub.milionerzy.MainActivity;
 import com.example.jakub.milionerzy.R;
 import com.example.jakub.milionerzy.entity.CategoryEntity;
+import com.example.jakub.milionerzy.utils.GetUrlContentTask;
 
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Created by jakub on 30.10.2018.
@@ -33,58 +29,44 @@ public class CategoryActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.category);
 
-        String readFeed = readFeed();
-
         ArrayList<CategoryEntity> categories = new ArrayList<CategoryEntity>();
         ArrayList<String> categoryName = new ArrayList<String>();
-
         try {
-            JSONObject json = new JSONObject(readFeed);
-            JSONArray jsonArray = new JSONArray(json.optString("categories"));
+            String response = new GetUrlContentTask().execute("http://10.0.2.2:8080/category/get").get();
 
-            for (int i = 0; i < jsonArray.length(); i++) {
-                JSONObject jsonObject = jsonArray.getJSONObject(i);
+            if (response.isEmpty()) {
+                getAlertDialog();
+            } else {
+                JSONArray jsonArray = new JSONArray(response);
 
-                CategoryEntity categoryEntity = new CategoryEntity();
-                categoryEntity.setName(jsonObject.optString("name"));
-                categoryEntity.setId(jsonObject.optInt("id"));
-                categories.add(categoryEntity);
-                categoryName.add(jsonObject.optString("name"));
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    CategoryEntity categoryEntity = new CategoryEntity();
+                    categoryEntity.setName(jsonObject.optString("name"));
+                    categoryEntity.setId(jsonObject.optInt("id"));
+                    categories.add(categoryEntity);
+                    categoryName.add(jsonObject.optString("name"));
+                }
 
+                Spinner spinner = (Spinner) findViewById(R.id.spinnerCategories);
+                spinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, categoryName));
             }
-        } catch (Exception e) {
+        } catch (InterruptedException | ExecutionException | JSONException e) {
             e.printStackTrace();
         }
-        Spinner spinner = (Spinner) findViewById(R.id.spinnerCategories);
-        spinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, categoryName));
-
     }
 
-    public String readFeed() {
-        StringBuilder builder = new StringBuilder();
-//        HttpClient client = new DefaultHttpClient();
-//
-//        HttpGet httpGet = new HttpGet("http://localhost:8080/category/get");
-//        try {
-//            HttpResponse response = client.execute(httpGet);
-//            StatusLine statusLine = response.getStatusLine();
-//            int statusCode = statusLine.getStatusCode();
-//            if (statusCode == 200) {
-//                HttpEntity entity = response.getEntity();
-//                InputStream content = entity.getContent();
-//                BufferedReader reader = new BufferedReader(new InputStreamReader(content));
-//                String line;
-//                while ((line = reader.readLine()) != null) {
-//                    builder.append(line);
-//                }
-//            } else {
-//                Log.e(CategoryActivity.class.toString(), "Failed to download lists categories");
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-        return builder.toString();
-
+    public void getAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Brak kategorii");
+        builder.setMessage("Nie dodatno aktualnie żadnych kategorii");
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                CategoryActivity.this.finish();
+            }
+        }).show();
+        AlertDialog alert = builder.create();
+        alert.getWindow().setLayout(500, 600);
     }
 
 }
---
+++ app/src/main/java/com/example/jakub/milionerzy/utils/GetUrlContentTask.java
@@ -0,0 +1,42 @@
+package com.example.jakub.milionerzy.utils;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by jakub on 30.10.2018.
+ */
+
+public class GetUrlContentTask extends AsyncTask<String, Integer, String> {
+    @Override
+    protected String doInBackground(String... urls) {
+        StringBuilder content = new StringBuilder();
+        String line;
+        try {
+            URL url = new URL(urls[0]);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Content-Type", "*/*");
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+            connection.connect();
+            if (connection.getResponseCode() == 200) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                while ((line = reader.readLine()) != null) {
+                    content.append(line).append("\n");
+                }
+            }else{
+                System.err.println("Response code: " + connection.getResponseCode());
+            }
+
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
+        return content.toString();
+    }
+}
